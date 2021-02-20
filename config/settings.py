09ec5c6888c19e6f2c5839129050f1ddb235957b
zelnkup <i.stebelskuy@gmail.com>
2021-02-20 20:37:06 +0200

    fixes

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # 'users.middlewares.AuthRequiredMiddleware',
+    "users.middlewares.AuthRequiredMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
